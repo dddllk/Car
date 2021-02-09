3cf1ec2f21ef52f4b90bac368a53715abc3ccf9a
dlkyy <474464152@qq.com>
2021-02-09 20:30:29 +0800

    JDBC003-sql查询

--- 04-JDBC开发演示/src/com/bjpowernode/test/JDBCTest03.java
+++ 04-JDBC开发演示/src/com/bjpowernode/test/JDBCTest03.java
@@ -2,6 +2,8 @@ package com.bjpowernode.test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 /**
  * Created by dlkyy on 2021-02-09 13:45
@@ -14,5 +16,26 @@ public class JDBCTest03 {
 		Class.forName("com.mysql.jdbc.Driver");
 		// 2.建立连接
 		Connection con = DriverManager.getConnection(url, "root", "123456");
+		// 3.创建连接工具
+		PreparedStatement ps = con.prepareStatement("");
+		// 4.执行sql
+		ResultSet rs = ps.executeQuery(sql);
+		// 5.遍历,读取信息
+		while(rs.next()){
+			int deptno = rs.getInt("deptno");
+			String dname = rs.getString("dname");
+			String location = rs.getString("loc");
+			System.out.println(deptno + "|" + dname + "|" + location);
+		}
+		// 6.销毁资源
+		if(rs != null){
+			rs.close();
+		}
+		if(ps != null){
+			ps.close();
+		}
+		if(con != null){
+			con.close();
+		}
 	}
 }
